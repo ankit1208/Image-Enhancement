383f18aabf14c318c0205b5694c095861b306a50
Ankit Choudhary <58726499+ankit1208@users.noreply.github.com>
2023-04-02 14:10:50 +0530

    changes

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+# from PIL import Image
+# import joblib
+
+# import pickle
+import os
+import demo
+import time
+import glob
+
+
+
+app = Flask(__name__)
+
+# model = joblib.load('demo.py')
+
+def get_latest_file(path,*paths):
+    """Returns the name of the latest (most recent) file 
+    of the joined path(s)"""
+    fullpath = os.path.join(path,*paths)
+    files = glob.glob(fullpath)  # You may use iglob in Python3
+    if not files:                # I prefer using the negation
+        return None                      # because it behaves like a shortcut
+    latest_file = max(files, key=os.path.getctime)
+    _, filename = os.path.split(latest_file)
+    return filename
+
+# Define a route to handle the request
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    if request.method == 'POST':
+        f = request.files['file']
+        os.chdir('C:\\Users\\ASUS\\Desktop\\beproj\\Image-Enhancement\\samples\\input')
+        f.save(f.filename)
+        os.chdir('C:\\Users\\ASUS\\Desktop\\beproj\\Image-Enhancement')
+        demo.demo()
+        fname=f.name
+        print(fname)
+        # time.sleep(9)
+        # path=f'samples\output\img1.png'
+        # out=demo.ocr(path)
+        # print(out)
+        return 'marja'
+    elif request.method=="GET":
+        # folder_path = r'samples\output'
+        # file_type = r'*png'
+        # files = glob.glob(folder_path + file_type)
+        # max_file = max(files, key=os.path.getctime)
+
+        # print(max_file)
+        fname=get_latest_file('samples\output','*png')
+        print(fname)
+        path=f'samples\output\{fname}'
+        out=demo.ocr(path)
+        print(out)
+        return out
+
+
+if __name__ == "__main__":
+    app.run(debug = "True",port=3000)
--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+# from flask import Flask
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,81 +13,120 @@ from glob import glob
 import cv2
 import argparse
 import gc
+import os
+import io
+from google.cloud import vision
+from google.cloud.vision_v1 import types
 gc.collect()
 torch.cuda.empty_cache()
+
+# app = Flask(__name__)
+
 #torch.cuda.empty_cache()
 #torch.cuda.memory_summary(device=None, abbreviated=False)
-parser = argparse.ArgumentParser(description='Demo MPRNet')
-parser.add_argument('--input_dir', default='./samples/input/', type=str, help='Input images')
-parser.add_argument('--result_dir', default='./samples/output/', type=str, help='Directory for results')
-parser.add_argument('--task', required=True, type=str, help='Task to run', choices=['Deblurring'])#, 'Denoising', 'Deraining'])
+# parser = argparse.ArgumentParser(description='Demo MPRNet')
+# parser.add_argument('--input_dir', default='./samples/input/', type=str, help='Input images')
+# parser.add_argument('--result_dir', default='./samples/output/', type=str, help='Directory for results')
+# parser.add_argument('--task', required=True, type=str, help='Task to run', choices=['Deblurring'])#, 'Denoising', 'Deraining'])
 
-args = parser.parse_args()
+# args = parser.parse_args()
+def demo():
+    def save_img(filepath, img):
+        cv2.imwrite(filepath,cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
-def save_img(filepath, img):
-    cv2.imwrite(filepath,cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    def load_checkpoint(model, weights):
+        checkpoint = torch.load(weights)
+        try:
+            model.load_state_dict(checkpoint["state_dict"])
+        except:
+            state_dict = checkpoint["state_dict"]
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k[7:] # remove `module.`
+                new_state_dict[name] = v
+            model.load_state_dict(new_state_dict)
 
-def load_checkpoint(model, weights):
-    checkpoint = torch.load(weights)
-    try:
-        model.load_state_dict(checkpoint["state_dict"])
-    except:
-        state_dict = checkpoint["state_dict"]
-        new_state_dict = OrderedDict()
-        for k, v in state_dict.items():
-            name = k[7:] # remove `module.`
-            new_state_dict[name] = v
-        model.load_state_dict(new_state_dict)
+    task    = 'Deblurring'
+    inp_dir ='./samples/input/'
+    out_dir = './samples/output/'
 
-task    = args.task
-inp_dir = args.input_dir
-out_dir = args.result_dir
+    os.makedirs(out_dir, exist_ok=True)
 
-os.makedirs(out_dir, exist_ok=True)
+    files = natsorted(glob(os.path.join(inp_dir, '*.jpg'))
+                    + glob(os.path.join(inp_dir, '*.JPG'))
+                    + glob(os.path.join(inp_dir, '*.png'))
+                    + glob(os.path.join(inp_dir, '*.PNG')))
 
-files = natsorted(glob(os.path.join(inp_dir, '*.jpg'))
-                + glob(os.path.join(inp_dir, '*.JPG'))
-                + glob(os.path.join(inp_dir, '*.png'))
-                + glob(os.path.join(inp_dir, '*.PNG')))
+    if len(files) == 0:
+        raise Exception(f"No files found at {inp_dir}")
 
-if len(files) == 0:
-    raise Exception(f"No files found at {inp_dir}")
+    # Load corresponding model architecture and weights
+    load_file = run_path(os.path.join(task, "MPRNet.py"))
+    model = load_file['MPRNet']()
+    model.cuda()
 
-# Load corresponding model architecture and weights
-load_file = run_path(os.path.join(task, "MPRNet.py"))
-model = load_file['MPRNet']()
-model.cuda()
+    weights = os.path.join(task, "pretrained_models", "model_"+task.lower()+".pth")
+    load_checkpoint(model, weights)
+    model.eval()
 
-weights = os.path.join(task, "pretrained_models", "model_"+task.lower()+".pth")
-load_checkpoint(model, weights)
-model.eval()
+    img_multiple_of = 8
 
-img_multiple_of = 8
+    for file_ in files:
+        img = Image.open(file_).convert('RGB')
+        input_ = TF.to_tensor(img).unsqueeze(0).cuda()
 
-for file_ in files:
-    img = Image.open(file_).convert('RGB')
-    input_ = TF.to_tensor(img).unsqueeze(0).cuda()
+        # Pad the input if not_multiple_of 8
+        h,w = input_.shape[2], input_.shape[3]
+        H,W = ((h+img_multiple_of)//img_multiple_of)*img_multiple_of, ((w+img_multiple_of)//img_multiple_of)*img_multiple_of
+        padh = H-h if h%img_multiple_of!=0 else 0
+        padw = W-w if w%img_multiple_of!=0 else 0
+        input_ = F.pad(input_, (0,padw,0,padh), 'reflect')
 
-    # Pad the input if not_multiple_of 8
-    h,w = input_.shape[2], input_.shape[3]
-    H,W = ((h+img_multiple_of)//img_multiple_of)*img_multiple_of, ((w+img_multiple_of)//img_multiple_of)*img_multiple_of
-    padh = H-h if h%img_multiple_of!=0 else 0
-    padw = W-w if w%img_multiple_of!=0 else 0
-    input_ = F.pad(input_, (0,padw,0,padh), 'reflect')
+        with torch.no_grad():
+            model.eval()
+            restored = model(input_)
+        restored = restored[0]
+        restored = torch.clamp(restored, 0, 1)
 
-    with torch.no_grad():
-        model.eval()
-        restored = model(input_)
-    restored = restored[0]
-    restored = torch.clamp(restored, 0, 1)
+        # Unpad the output
+        restored = restored[:,:,:h,:w]
 
-    # Unpad the output
-    restored = restored[:,:,:h,:w]
+        restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
+        restored = img_as_ubyte(restored[0])
 
-    restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
-    restored = img_as_ubyte(restored[0])
+        f = os.path.splitext(os.path.split(file_)[-1])[0]
+        save_img((os.path.join(out_dir, f+'.png')), restored)
 
-    f = os.path.splitext(os.path.split(file_)[-1])[0]
-    save_img((os.path.join(out_dir, f+'.png')), restored)
+    print(f"Files saved at {out_dir}")
 
-print(f"Files saved at {out_dir}")
+def ocr(path):
+    #the JSON file you downloaded in step 5 above
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'high-theme-381806-e676f023df9a.json'
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+
+    # # set this thumbnail as the url
+    # image = types.Image()
+
+    # image.source.image_uri = '/content/testimage.jpeg'
+    # path='samples\output\img1.png'
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+        image = vision.Image(content=content)
+
+
+    #### TEXT DETECTION ######
+
+    response_text = client.text_detection(image=image)
+    # print(response_text)
+    # d = ""
+    str = response_text.text_annotations[0].description
+    return str
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug = "True",port=3000)
